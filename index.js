e87126883898312346f91c0b51a5d18a94390d14
AyrozDZN <34416779+AyrozDZN@users.noreply.github.com>
2018-07-23 17:34:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ client.on('guildMemberRemove', member => {
 client.on('message', async message => {
   
     if(message.content.startsWith(prefix + "avatar")) {
-        message.send(author.avatarURL)
+        message.send(message.author.avatarURL)
     }
     
     if (message.content === prefix + "help") {
